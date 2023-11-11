8c10cde7cc822b6f5783173cadd145577da0f64a
Denis Popkov <popkov-den03@mail.ru>
2023-11-11 18:42:05 +0600

    add: header view

---
+++ DoctorApp/components/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  DoctorApp
+//
+//  Created by Denis Popkov on 11/11/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
