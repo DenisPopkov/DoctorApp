01360e0236d84677c80f47730553b6f3011f0062
Denis Popkov <d.popkov@ltst.ru>
2023-11-11 17:15:16 +0600

    Initial Commit

---
+++ DoctorApp/DoctorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorAppApp.swift
+//  DoctorApp
+//
+//  Created by Denis Popkov on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DoctorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
