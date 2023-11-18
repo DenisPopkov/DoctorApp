8cfadf90a50661bfd382e9ef3f0a749d76d15958
Denis Popkov <popkov-den03@mail.ru>
2023-11-18 09:44:57 +0600

    fix: category view spacing

--- DoctorApp/Views/MainScreen/Components/CategoryView.swift
+++ DoctorApp/Views/MainScreen/Components/CategoryView.swift
@@ -12,20 +12,20 @@ struct CategoryView: View {
     @Binding var categoryList: [CategoryModel]
     
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: itemSpacing) {
-                ForEach(categoryList, id: \.self) { category in
-                    Button(action: {}) {
-                        VStack {
-                            Image(category.imageUrl)
-                                .padding(largePadding)
-                                .background(.accentBackground, in: .circle)
-                            Text(LocalizedStringKey(category.categoryName))
-                                .foregroundColor(.purple)
-                                .font(.poppinsRegular16)
-                        }
-                    }.hoverEffect()
-                }
+        HStack {
+            ForEach(categoryList, id: \.self) { category in
+                Button(action: {}) {
+                    VStack {
+                        Image(category.imageUrl)
+                            .frame(maxWidth: .infinity)
+                            .padding(largePadding)
+                            .background(.accentBackground, in: .circle)
+                        Text(LocalizedStringKey(category.categoryName))
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.purple)
+                            .font(.poppinsRegular16)
+                    }.fixedSize(horizontal: false, vertical: true)
+                }.hoverEffect()
             }
         }
     }
